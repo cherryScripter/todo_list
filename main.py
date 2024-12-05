1a6d8945eccdefe26e0d0e733ed379de804b4611
cherryScripter <kreska323@gmail.com>
2024-12-05 17:46:20 +0100

    Fixed bugs with port

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from flask_bootstrap import Bootstrap5
 import os
 from dotenv import load_dotenv
 from flask_wtf.csrf import CSRFError
+import os
 
 load_dotenv()
 
@@ -116,4 +117,5 @@ def delete() -> Response:
 
 
 if __name__ == "__main__":
-    app.run(debug=False, host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host="0.0.0.0", port=port)
